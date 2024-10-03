9c906c5e68e6fd568150c9423d21c4bf45fa56b1
Oladipupo Olubunmi <74682430+Bunmi2020@users.noreply.github.com>
2024-10-03 17:59:33 +0100

    friday predictions in

--- src/week7/day_fixture/friday.js
+++ src/week7/day_fixture/friday.js
@@ -37,7 +37,7 @@ function Friday ({ setContent }) {
     return (
         <div id="days_menu" className="days_menu">
             <h5 id='friday'>
-                27/09 - Friday 
+                04/10 - Friday 
             </h5>
             <ul className="all_fixtures">
                         {menuItems.map((match, index) => (
