fb09894eba39d937a1435079d9bc35826d055d9a
Oladipupo Olubunmi <74682430+Bunmi2020@users.noreply.github.com>
2025-02-21 18:42:48 +0100

    new week data predictions updated

--- src/week19/league_menu/serie_a.js
+++ src/week19/league_menu/serie_a.js
@@ -10,7 +10,7 @@ function SerieAMenu({ setContent }) {
     const [arrow, setArrow] = useState(down);
     
     useEffect(() => {
-        const apiUrl = 'https://bunmi2020.github.io/bnf_data/week_eighteen/serie_a.json';
+        const apiUrl = 'https://bunmi2020.github.io/bnf_data/week_nineteen/serie_a.json';
 
         fetch(apiUrl, {
             method: 'GET',
